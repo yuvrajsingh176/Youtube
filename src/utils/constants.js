5370386eb85bade23f2fdf7faf2af123177ee81a
YUVRAJ SINGH <yyuvrajssingh2001@gmail.com>
2024-01-08 12:39:15 +0530

    added changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const gooleapikey="AIzaSyAfGp5ae9dA1rASCfR1o9ncKvx_kngVk6Y"
 export const youtubeapi = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +gooleapikey;
 
-export const youtube_search_api = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const youtube_search_api = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
 export const offsetLiveChat = 100;
 
